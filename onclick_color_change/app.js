63c7f7e63bc22380f25bd39eff04111a094538e5
Venicherla Bhargavi <123863188+Bhargavi-145@users.noreply.github.com>
2024-01-13 12:36:44 +0530

    app.js

---
+++ onclick_color_change/app.js
@@ -0,0 +1,14 @@
+function change_color_1() {
+    document.getElementById("text").style.color = "red";
+    console.log("color changed to red!!");
+}
+
+function change_color_2() {
+    document.getElementById("text").style.color = "blue";
+    console.log("color changed to blue!!");
+}
+
+function change_color_3() {
+    document.getElementById("text").style.color = "green";
+    console.log("color changed to green!!");
+}
